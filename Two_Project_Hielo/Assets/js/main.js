ecfd5db6ced0d126f8a7451ae938ce48c5a04c11
Vando dos reis <vandodosreis2001@gmail.com>
2021-02-08 23:39:09 -0300

    Pointers quase pronto

--- Two_Project_Hielo/Assets/js/main.js
+++ Two_Project_Hielo/Assets/js/main.js
@@ -18,19 +18,35 @@ function fechamenu() {
     someMenu.style.display = 'flex'
   }
 }
-/*
-let sliderPointers = document.querySelector('.sliders-pointers')
-let pointer = document.querySelectorAll('.banner .pointer');
 
-pointer[1].addEventListener('click', () => {
-    if(pointer[1].classList.contains('active') === false) {
-      pointer[1].classList.add('active');
-      pointer[0].classList.remove('active');
-    }else {
-      pointer[0].classList.add('active');
-      pointer[1].classList.remove('active');
+let totalSlider = document.querySelector('.sliders')
+
+let slider1 = document.querySelector('.slide1');
+let slider2 = document.querySelector('.slide2');
+let slider3 = document.querySelector('.slide3');
+let slider4 = document.querySelector('.slide4');
+let slider5 = document.querySelector('.slide5');
+
+let sliders = [ slider1, slider2, slider3, slider4, slider5];
+
+let sliderPointers = document.querySelector('.sliders-pointers')
+let pointer = document.querySelectorAll('.pointer');
+let qtPonto = false;
+
+pointer.forEach((ponto, index) => {
+  ponto.addEventListener('click', () => {
+    if(ponto.classList.contains('active') === true) {
+      ponto.classList.remove('active');
+      sliders[index].style.zIndex = 4;
+    } else {
+      sliders[index].previousElementSibling.style.zIndex = 4;
+      ponto.previousElementSibling.classList.remove('active');
+      ponto.classList.add('active');
+      sliders[index].style.zIndex = 5;
     }
-  
+  });
 });
-*/
+
+
+console.log(totalSlider.children)
 
